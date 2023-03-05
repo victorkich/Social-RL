858ccd3e93e36b59b95beb0be747f7bb3c2ecd8b
victorkich <victorkich@yahoo.com.br>
2023-03-05 01:05:32 -0300

    Update

--- social_rl.py
+++ social_rl.py
@@ -178,11 +178,11 @@ class Environment:
         active_locks = np.array(self.active_locks)
         print(active_locks)
         for i in range(4):
-            if i == 0 and any(active_locks[[0, 1]]):
+            if i == 0 and any(active_locks[[0]]):
                 c_lock = 140
             elif i == 1 and any(active_locks[[1, 2]]):
                 c_lock = 140
-            elif i == 2 and any(active_locks[[0 , 3]]):
+            elif i == 2 and any(active_locks[[1, 3]]):
                 c_lock = 140
             elif i == 3 and any(active_locks[[2, 3]]):
                 c_lock = 140
